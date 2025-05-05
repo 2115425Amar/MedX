5bed80bb59c42553f5cddd6260c4169d1b426d44
2115425Amar <amar2115425@akgec.ac.in>
2025-05-05 12:57:32 +0530

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Only use :id for inspections in production.
-  config.active_record.attributes_for_inspect = [ :id ]
+  # config.active_record.attributes_for_inspect = [ :id ]
 
   config.secret_key_base = ENV["SECRET_KEY_BASE"]
 
